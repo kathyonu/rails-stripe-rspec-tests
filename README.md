d75d8521814c6f75118923e85a62ed99ca7cb309
Will Schive <lexigram@goodworksonearth.org>
2015-06-09 00:44:55 -0700

    begin developing managed accounts tests

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rails-stripe-rspec-tests
-Testing framework including Stripe tests for membership, products, webhoooks, etc.
+### Testing framework including (eventually) all Stripe processes tested
+##### for user interaction for subscription memberships, products sales, webhoooks, etc.
+
+This respository of Tests Only is being built specifically for testing our live in production app.
+These tests are designed to test the Ruby 2.2.2 on Rails 4.2.1 rails-stripe-memberships-saas using only Stripe codes, Stripe tests, Stripe webhooks.
+
+The original 3.2 branch of the membership app by [Daniel Kehoe](https://github.com/RailsApps) can be found there. We have developed this old and pure Stripe application to current codes, and now we share the tests.
---
+++ spec/stripe/stripe_config_spec.rb
@@ -0,0 +1,22 @@
+describe "Config Variables" do
+
+  describe "STRIPE_API_KEY" do
+    # this tests env variables against config/secrets.yml which reads from your env variables $ env
+    # to set this env variable $ export STRIPE_API_KEY=yourverylongStripApiKey
+    # to set this heroku config variable in production $ heroku config:add STRIPE_API_KEY=sk_live_yourlongstring
+    it "a STRIPE_API_KEY is set" do
+      expect(ENV['STRIPE_API_KEY']).to eq(Rails.application.secrets.stripe_api_key),
+        "Your STRIPE_API_KEY is not set, Please refer to the 'Configure the Stripe Initializer' section of the README"
+    end
+  end
+
+  describe "STRIPE_PUBLISHABLE_KEY" do
+    # to set this env variable $ export STRIPE_PUBLISHABLE_KEY=yourverylongStripPublishableKey
+    # to set this heroku config variable in production $ heroku config:add STRIPE_PUBLISHABLE_KEY=pk_live_yourlongstring
+    it "a STRIPE_PUBLISHING_KEY is set" do
+      expect(ENV['STRIPE_PUBLISHABLE_KEY']).to eq(Rails.application.secrets.stripe_publishable_key),
+        "Your STRIPE_PUBLISHABLE_KEY is not set, Please refer to the 'Configure the Stripe Initializer' section of the README"
+    end
+  end
+
+end
