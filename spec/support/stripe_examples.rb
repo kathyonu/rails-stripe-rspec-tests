cfb26d6ed9eeb1a1f98746614888552bef0428c7
Will Schive <lexigram@goodworksonearth.org>
2015-08-11 08:35:28 -0700

    restored stripe_examples.rb

--- spec/support/stripe_examples.rb
+++ spec/support/stripe_examples.rb
@@ -1,6 +1,11 @@
-=begin
-for future use
+
+def require_stripe_examples
+  Dir["./spec/shared_stripe_examples/**/*.rb"].each {|f| require f}
+  Dir["./spec/integration_examples/**/*.rb"].each {|f| require f}
+end
+
 def it_behaves_like_stripe(&block)
+  it_behaves_like 'Account API', &block
   it_behaves_like 'Bank Account Token Mocking', &block
   it_behaves_like 'Card Token Mocking', &block
   it_behaves_like 'Card API', &block
@@ -13,6 +18,7 @@ def it_behaves_like_stripe(&block)
   it_behaves_like 'Plan API', &block
   it_behaves_like 'Recipient API', &block
   it_behaves_like 'Refund API', &block
+  it_behaves_like 'Transfer API', &block
   it_behaves_like 'Stripe Error Mocking', &block
   it_behaves_like 'Customer Subscriptions', &block
   it_behaves_like 'Webhook Events API', &block
@@ -21,4 +27,3 @@ def it_behaves_like_stripe(&block)
   it_behaves_like 'Multiple Customer Cards'
   it_behaves_like 'Charging with Tokens'
 end
-=end
