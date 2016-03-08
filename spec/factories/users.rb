fb6ec6964ca7e0c26d0fb29a79ae21a41351e1f6
Will Schive <lexigram@goodworksonearth.org>
2016-03-08 08:11:42 -0800

    cleaned up user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,18 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+# Traits allow you to group attributes together and then apply them to any factory.
+# Reference : https://github.com/thoughtbot/factory_girl/blob/master/GETTING_STARTED.md
+
 FactoryGirl.define do
   factory :user do
-    email "test@example.com"
-    password "please123"
+    name 'Test User'
+    email 'test@example.com'
+    password '8charmin'
+    password_confirmation '8charmin'
+    # required if the Devise Confirmable module is used
+    # confirmed_at Time.now
 
     trait :admin do
       role 'admin'
     end
-
   end
 end
