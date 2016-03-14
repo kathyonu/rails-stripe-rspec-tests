72405775e65d6a0c6a5b10929b43cf65677302ee
Will Schive <lexigram@goodworksonearth.org>
2016-03-13 21:32:07 -0700

    added modal_helpers

---
+++ spec/support/helpers/modal_helpers.rb
@@ -0,0 +1,11 @@
+# accesses modal popup windows
+module ModalHelpers
+  def switch_to_new_pop_up
+    win = page.driver.browser.switch_to.window(page.driver.browser.window_handles.last)
+  end
+
+  def close_active_window
+    page.driver.browser.close  
+    page.driver.browser.switch_to.window(page.driver.browser.window_handles[0])
+  end
+end
