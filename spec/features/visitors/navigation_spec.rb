b6a41d044ee879fe7a1ea962421e3c52863bf474
Will Schive <lexigram@goodworksonearth.org>
2016-03-15 00:19:56 -0700

    passing navigation tests

--- spec/features/visitors/navigation_spec.rb
+++ spec/features/visitors/navigation_spec.rb
@@ -1,8 +1,11 @@
-include Warden::Test::Helpers 
+include Warden::Test::Helpers
 Warden.test_mode!
 
-RSpec.describe 'Navigation links' do
-
+# Feature: Navigation links
+#   As a visitor
+#   I want to see navigation links
+#   So I can find home, sign in, or sign up
+feature 'Navigation links' do
   before(:each) do
     visit root_path
   end
@@ -11,29 +14,48 @@ RSpec.describe 'Navigation links' do
     Warden.test_reset!
   end
 
-  it 'allows visitor to arrive on the home page' do
+  # Scenario: View navigation links
+  #   Given I am a visitor
+  #   When I visit the home page
+  #   Then I see Home, Sign in, and Sign up
+  scenario 'view navigation links' do
+    # visit root_path
+    # expect(page).to have_content 'Home'
+    expect(page).to have_content 'Sequencers Membership'
+    expect(page).to have_content 'Login'
+    # expect(page).to have_content 'Sign in'
+    # expect(page).to have_content 'Register'
+    expect(page).to have_content 'Sign Up'
+  end
+
+  scenario 'allows visitor to arrive on the home page' do
     expect(current_path).to eq root_path
   end
 
-  it 'allows visitor to view navigation links' do
-    expect(page).to have_link 'Home'
-    expect(page).to have_link 'Sign in'
+  scenario 'allows visitor to view navigation links' do
+    expect(page).to have_content 'Welcome'
+    # expect(page).to have_link 'Home'
+    expect(page).to have_link 'Database Info'
+    expect(page).to have_link 'MailList Info'
+    expect(page).to have_link 'Login'
+    # expect(page).to have_link 'Register'
+    # expect(page).to have_link 'Sign in'
     expect(page).to have_link 'Sign up'
   end
 
-  it 'allows visitor to click on the Sign up link' do
-    click_link("Sign up")
+  scenario 'allows visitor to click on the Sign up link' do
+    visit root_path
+    click_link 'Sign up'
     expect(current_path).to eq '/users/sign_up'
   end
 
-  it 'allows visitor to type a correct path in the address bar' do
+  scenario 'allows visitor to type a correct path in the address bar' do
     visit '/users/sign_up?plan=gold'
     expect(current_path).to eq '/users/sign_up'
   end
 
-  it 'allows visitor to arrive on sign_up page when typing an incorrect plan in the address bar' do
+  scenario 'allows visitor to arrive on sign_up page when typing an incorrect plan in the address bar' do
     visit '/users/sign_up?plan=hobo'
     expect(current_path).to eq '/users/sign_up'
   end
-
 end
