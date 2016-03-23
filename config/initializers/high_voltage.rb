a9adc28a02b115e0072216fb6554618d9988610f
Will Schive <lexigram@goodworksonearth.org>
2016-03-23 00:10:17 -0700

    instruction improvement

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -1,7 +1,7 @@
 # config/initializers/high_voltage.rb
-## enables use of address without pages in it
-## http://goodworksonearth.net/about vs 
-## http://goodworksonearth.net/pages/about
+# enables use of address without pages in it
+# http://www.example.com/about versus
+# http://www.example.com/pages/about
 
 HighVoltage.configure do |config|
   config.route_drawer = HighVoltage::RouteDrawers::Root
