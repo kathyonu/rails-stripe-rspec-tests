8e93007f813f5ce66edd141d874cf6bb427fb259
Will Schive <lexigram@goodworksonearth.org>
2016-03-24 23:09:29 -0700

    passing tests

--- spec/views/visitor_index_spec.rb
+++ spec/views/visitor_index_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 include Warden::Test::Helpers
 Warden.test_mode!
 
@@ -5,18 +6,28 @@ Warden.test_mode!
 # As a new visitor to the site
 # I want to sign up with my email
 # Then gain access to the free Ebook
-RSpec.describe Visitor do
-
+RSpec.describe Visitor, type: :view do
   before(:each) do
-    @visitor = FactoryGirl.create(:visitor)
+    @visitor = FactoryGirl.build(:visitor, email: 'visitor@example.com')
   end
 
   after(:each) do
     Warden.test_reset!
   end
 
+  it 'displays the visitor new page' do
+    visit root_path
+    expect(current_path).to eq '/' # == /visitors/new
+  end
+
+  # also fully passing with real email address
   it '#subscribe' do
+    expect(@visitor.persisted?).to eq false
+    expect(@visitor.email).to eq('visitor@example.com')
+
+    @visitor.save!
+    expect(@visitor.persisted?).to be true
     expect(@visitor.subscribe).to eq true
-    expect(@visitor.email).to eq("visitor@example.com")
+    expect(@visitor.email).to eq('visitor@example.com')
   end
 end
